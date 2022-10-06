c5c2ffc815ba16ce7b5d82f3154b96f8e5b53f9e
TheLegendary1248 <1248alex@gmail.com>
2022-10-05 20:21:21 -0400

    Better equipped for build

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
+//Modules
+require('dotenv').config()
 const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
-
+// serve static front end in production mode
+if (process.env.NODE_ENV === "production") {
+  console.log("Using production mode")
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
+}
+app.get('/main', function(req,res){
+  res.send('This is the main page')
+})
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(9000);
+app.get('*', (req, res) => {
+  console.log("Failed to get page")
+  res.send("404 Failed to get page")
+})
+
+app.listen(process.env.PORT, function(err){
+    if (err) console.log(err);
+    console.log("Server listening on PORT", process.env.PORT);
+});
