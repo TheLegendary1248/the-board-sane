2921b3731108c3997fd8dfa4aa28fb16bb9e1a89
TheLegendary1248 <1248alex@gmail.com>
2022-10-22 21:30:40 -0400

    CSS for new board modal

--- src/views/boardSelect.jsx
+++ src/views/boardSelect.jsx
@@ -1,18 +1,17 @@
 //The board selection of the application
 import React, { useEffect } from 'react'
-import Card  from './components/boardCard.jsx'
+import Card from './components/boardCard.jsx'
 
 //Container for boards.
 let boardCont = <p>Getting boards...</p>
 
-async function hello(){
-    
+async function hello() {
+
 }
 //TODO Allow this page to be accessed without signing up. Use cookies to save board info for unregistered users
-function BoardSelect(data)
-{
+function BoardSelect(data) {
     let m;
-    import('../styles/boardSelect.css').then((e) => {m = e.default; console.log("Vars:",m,e)})
+    import('../styles/boardSelect.css').then((e) => { m = e.default; console.log("Vars:", m, e) })
     let boards = []
     /*
     //let get = await fetch("https://localhost:8000/board")
@@ -23,10 +22,10 @@ function BoardSelect(data)
     })*/
     useEffect(() => {
         console.log("Hi")
-        return () => {console.log("Before:", m); m = null; console.log("After:", m);}
+        return () => { console.log("Before:", m); m = null; console.log("After:", m); }
     }, [])
     if (boards.length == 0) boards = <h3>You currently have no boards</h3>
-    return(
+    return (
         <div id="R_select">
             <h1>Your Boards</h1>
             <div id="boardSelect">
@@ -34,14 +33,17 @@ function BoardSelect(data)
             </div>
             {/**Add two 'absolute' divs here, one to disappear on hover via css and vice versa*/}
             <div id="addBoard">
-                <h2 id="addHeader">Create a new Board</h2>
-                <form method="POST" action="">
-                    <label htmlFor="new_title" hidden={true}>New title of board</label>
-                    <input name="name" id="new_title" type="text" placeholder="Board Title"/>
-                    <label for="new_isOffline">Offline</label>
-                    <input id="new_isOffline" type="checkbox"/>
-                    <input id="create_new" type="submit" value="Create"/> 
-                </form>
+                <div id="onHover">
+                    <h2 id="addHeader">Create a new Board</h2>
+                    <form method="POST" action="">
+                        <label htmlFor="new_title" hidden={true}>New title of board</label>
+                        <input name="name" id="new_title" type="text" placeholder="Board Title" />
+                        <label for="new_isOffline">Offline</label>
+                        <input id="new_isOffline" type="checkbox" />
+                        <input id="create_new" type="submit" value="Create" />
+                    </form>
+                </div>
+                <div id="offHover">+</div>
             </div>
         </div>
     )
